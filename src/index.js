1353c5f5341cae7071e22f31eb169071499b5d4f
lcaeserj <73782676+lcaeserj@users.noreply.github.com>
2020-11-01 15:12:40 +0100

    Test files for zingchart-team

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import RadialNetworkChart from "./radialNetworkTest/RadialNetworkChart";
 import SunburstTest from "./sunBurstTest/SunburstTest";
-import GraphsetTest from "./drillDownTest/GraphsetTest";
 
 
 ReactDOM.render(
-    <GraphsetTest/>,
+    <SunburstTest/>,
   document.getElementById('root')
 );
 
